cd70cd8ca9e9937e61f32ab03f9b0a36af3e7d8e
suhail01101984 <suhailmemon84@gmail.com>
2023-06-07 19:59:24 -0600

    commit IDE files

--- courseproject/dict.py
+++ courseproject/dict.py
@@ -1,1 +1,27 @@
-def
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+    print(f'Hi, suhailmemon')
+    mydict={"BD":"bigdata course", "ML":"ml course", "java":"java course"}
+    print(mydict["BD"])
+    print(mydict["ML"])
+    print(mydict["java"])
+    mydict["spark"]="spark course"
+    print(mydict["spark"])
+    print(mydict)
+    print(type(mydict))
+    course_exist=mydict.get("spark")
+    print(course_exist)
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/

a48c3783b6d30b3f31d83d809e4b7e90c24aa0d1
suhail01101984 <suhailmemon84@gmail.com>
2023-09-19 20:25:44 -0600

    test_persist.py

--- courseproject/test_persist.py
+++ courseproject/test_persist.py
@@ -1,11 +1,13 @@
 import unittest
+from processor import persist
 
 class PersistDataTest(unittest.TestCase):
-    def test_first_test(self):
-        self.assertEqual(3,3)
-    def test_second_test(self):
-        self.assertTrue("PYTHON".isupper())
-
+    def test_persist_read_from_pg(self):
+        pd=persist.PersistData("postgres")
+        courses=pd.read_from_pg("futurexschema.futurex_course_catalog")
+        first_element=courses[0]
+        self.assertEqual(first_element[1],'Hadoop Spark')
+        self.assertEqual(len(courses[0]),9)
 
 if __name__=="__main__":
     unittest.main()
